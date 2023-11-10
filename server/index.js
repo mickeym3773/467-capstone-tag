17e041f26438bd73f19555d952c61310791896a0
mwyattcs <wyattmic@oregonstate.edu>
2023-11-09 20:27:26 -0800

    Added Mongoose and CRUD operations for games

--- server/index.js
+++ server/index.js
@@ -1,10 +1,31 @@
 import 'dotenv/config';
 import express from 'express';
+import mongoose from 'mongoose';
 
+// import routes
+import game_routes from './routes/game_routes.js';
+
+
+// connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI);
+const db = mongoose.connection;
+
+db.once("open", () => {
+    console.log("Successfully connected to MongoDB using Mongoose!");
+});
+
+db.on("error", () => {
+    console.log("Error connecting to MongoDB using Mongoose!");
+});
+
+
+// set up express
 const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.json());
+
+app.use('/games', game_routes)
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
---
+++ server/utils/async_error_handler.js
@@ -0,0 +1,3 @@
+export const asyncErrorHandler = (fn) => (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+};
