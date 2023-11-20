8213ce399f449ab72533d04b4a0dca5dbbec604a
Merlin Mallory <merlin.mallory.coder@gmail.com>
2023-11-21 04:03:59 +0900

    Fixed styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import splash_img from '../img/splash_img.png'
 import Header from '../components/Header';
+import makeGamesImg from '../img/makegames1.jpeg';
+import playGamesImg from '../img/playgames1.jpeg';
 
 function Home() {
   const user = JSON.parse(localStorage.getItem('user'));
@@ -24,14 +26,10 @@ function Home() {
         <img src={splash_img} alt="Splash" className="home-splash-img" />
       </div>
 
-      <div className="home-select-container">
-        <h1>Choose Your Adventure!</h1>
-        <img src ="../img/makegames1.jpeg" alt="Make Games" />
-        <a href="/make-games" onClick={handleMakeGamesClick}>Make Games</a>
-        <br>
-        </br>
-        <Link to="/scroll">Play Games</Link>
-      </div>
+      <center>
+      <Link to="/make-games" onClick={handleMakeGamesClick}><img src={makeGamesImg} alt="Make Games" width="20%" height="20%" /></Link>
+      <Link to="/scroll"><img src={playGamesImg} alt="Play Games" width="20%" height="20%" /></Link>
+      </center>
 
       <footer>
         (footer content here)
